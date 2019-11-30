59c10de5309c31df4544a0c59ec720f7a3b71a02
CorbynL <corbynlangley@gmail.com>
2019-11-30 21:21:29 +0200

    removed unused code

--- server/app.js
+++ server/app.js
@@ -6,18 +6,8 @@ const app = express();
 
 const port = 3000;
 
-
 app.use(cors());
 
-
-// app.get('*', function(req, res) {
-//     res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-//   });
-
-
-// app.use("/", express.static(__dirname + '/../frontend/dist/'));
-
 app.use(express.static(__dirname + '/../frontend/dist/'));
-
 
 app.listen(port, () => console.log(`listens on port ${port}`));
